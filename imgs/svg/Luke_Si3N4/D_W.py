3caa5c3c88fb1d1bc70ec850a42e8d50881d8078
Z.H. YIN <fibomat@gmail.com>
2020-01-12 17:57:12 +0900

    org svg folder

---
+++ imgs/svg/Luke_Si3N4/D_W.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from scipy.optimize import fsolve
+from scipy.constants import c
+
+plt.rcParams.update({    
+    'font.size': 16,
+    'lines.linewidth': 0.8,
+    'figure.max_open_warning': 0,
+    'savefig.dpi':300,
+    'savefig.transparent': True,
+})
+
+ni = 2.029 # inner index
+no = 1.48 # outer index
+wl = 1.55
+step = 1e-2 # pm
+wl_list = [wl-step,wl,wl+step]
+
+w = np.arange(0.6,2.4,0.01)
+t = np.arange(0.6,1.0,0.01)
+
+filename = f'ni_{ni}_no_{no}_cwl_{wl}_w_{w[0]:.2F}_{w[-1]:.2F}_step{w[1]-w[0]:.2F}_t_{t[0]:.2F}_{t[-1]:.2F}_step{t[1]-t[0]:.2F}.txt'
+
+d_array = np.loadtxt(filename)
+X, Y = np.meshgrid(w,t)
+fig, ax = plt.subplots(dpi=200,figsize=(8,3))
+im = ax.imshow(d_array, interpolation='bilinear', origin='lower',
+                cmap=cm.rainbow,extent=(w[0],w[-1],t[0],t[-1]),aspect='auto')
+
+cs = ax.contour(d_array,np.array([-200,0,100]),#np.arange(-200,1600,200),
+                origin='lower',#cmap='flag',
+                colors='k',extent=(w[0],w[-1],t[0],t[-1]))
+sizex, sizey = 1.5, 0.8
+mark = True
+if mark:
+#     ax.hlines(xmin=w[0],xmax=sizex,y=sizey, ls='--', alpha=0.5)
+#     ax.vlines(ymax=sizey,ymin=t[0],x=sizex, ls='--', alpha=0.5)
+    ax.plot(sizex,sizey,'o',color='black')
+
+# cs.levels = [nf(val) for val in cs.levels]
+# ax.plot(2.0,0.8,'o',color='black')
+# plt.setp(cs.collections[1], linewidth=2)
+
+if plt.rcParams["text.usetex"]:
+    fmt = r'%.0f'
+else:
+    fmt = ' %.0f '
+ax.clabel(cs, cs.levels, fmt=fmt,inline=1)
+
+ax.set_xlabel('width ($\mathrm{\mu}$m)')
+ax.set_ylabel(r'thickness ($\mathrm{\mu}$m)')
+
+fig.savefig(f'{wl*1e3:.0F}_{mark}.svg',dpi=200,transparent=True,bbox_inches='tight')
+ax.set_title(r'$D_{W}$ at ' + f'{wl*1e3:.0F} nm (ps/(nm·km) )')
+fig.savefig(f'{wl*1e3:.0F}_{mark}_title.svg',dpi=200,transparent=True,bbox_inches='tight')
---
+++ imgs/svg/Luke_Si3N4/luke.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.constants import c, pi
+from scipy.misc import derivative
+from math import sqrt
+# from matplotlib import rcParams
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.constants import c, pi
+from scipy.misc import derivative
+from math import sqrt
+# from matplotlib import rcParams
+
+from cycler import cycler
+plt.rcParams.update({    
+    'font.size': 16,
+#     'ytick.labelsize':14,
+    'lines.linewidth': 0.8,
+    'figure.max_open_warning': 0,
+    'legend.frameon': False,
+    'figure.subplot.hspace': 0,
+    'savefig.dpi':300,
+    'axes.labelsize': 16,
+    'savefig.transparent': True,
+    'axes.grid': True,
+    'grid.linestyle': ':',
+    'axes.prop_cycle': cycler('color', 'bgrcmyk')
+#     'xtick.alignment': 'right'
+})
+
+# wl,nRaw = np.loadtxt('ellipso.txt').T
+# nSq = nRaw**2
+
+def sell(x, a, b, c):
+    return 1+a*x**2/(x**2+b)+c*x # a unitless, b nm^2
+
+# def sell(x, a, b):
+#     return 1+a*x**2/(x**2+b) # a unitless, b nm^2
+# popt, pcov = curve_fit(sell, wl, nSq)
+# n = lambda x: sqrt(sell(x,*popt))
+
+precision = 1e-1
+wlRange = np.arange(1300,1700,precision)
+
+def  Luke(x):
+    return sqrt(1+(3.0249*x**2)/(x**2-135.3406**2)+(40314*x**2)/(x**2-1239842**2))
+n_Luke = [Luke(i) for i in wlRange]
+d2n_Luke = [derivative(Luke, i, dx=5e-3, n=2) for i in wlRange]
+D_Luke = d2n_Luke*wlRange/c*(-1)*1e9
+
+fig, (axn,axD) = plt.subplots(2,1,figsize=(7,5),sharex=True)
+# axD = axn.twinx()
+
+axn.plot(wlRange,n_Luke, 'r-', label='Luke 2015')
+axD.plot(wlRange, D_Luke*1e6, 'b--',label=r'$D_{\lambda}$')
+
+
+axD.set_xlabel(r'wavelength (nm)')
+axD.set_ylabel(r'$D_M (ps/km·nm)')
+axn.set_ylabel('refractive index')
+
+
+pin = np.argmin(np.abs(wlRange-1550))
+axD.scatter(wlRange[pin], D_Luke[pin]*1e6,color='blue')
+axn.scatter(wlRange[pin], n_Luke[pin],color='red')
+
+# axn.annotate(f'({wlRange[pin]:.0f}, {n_Luke[pin]:.4f})', xy = (wlRange[pin]-200,n_Luke[pin]))
+# axD.annotate(f'({wlRange[pin]:.0f}, {D_Luke[pin]*1e6:.2f})', xy = (wlRange[pin]-200,D_Luke[pin]))
+
+# axD.tick_params(axis='y', labelcolor='blue')
+# axn.tick_params(axis='y', labelcolor='red')
+
+plt.savefig('Luke_raw.svg', dpi = 300, transparent=True, bbox_inches='tight')
